cc6d4ce642b186f33dc3e2f498c1465822775e0f
JorgeFernandez19 <134749317+JorgeFernandez19@users.noreply.github.com>
2024-07-10 20:19:49 -0600

    Vista de actualizar 10

--- app/src/main/java/com/example/app_market/DashboardClient.kt
+++ app/src/main/java/com/example/app_market/DashboardClient.kt
@@ -2,15 +2,23 @@ package com.example.app_market
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import storage.StoragePreferences
 
 class DashboardClient : AppCompatActivity() {
     private lateinit var btnShowPedidos: CardView
     private lateinit var btnNuevoPedido: CardView
+    private lateinit var btnLogut: LinearLayout
+    private val preference = StoragePreferences.getInstance(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -25,6 +33,23 @@ class DashboardClient : AppCompatActivity() {
         btnNuevoPedido.setOnClickListener {
             val intent = Intent(this, CatalogoProductosActivity::class.java)
             startActivity(intent)
+        }
+        btnLogut = findViewById(R.id.btcerrar)
+        btnLogut.setOnClickListener {
+            lifecycleScope.launch(Dispatchers.IO){
+                preference.deleteCredentials()
+            }
+        }
+        lifecycleScope.launch(Dispatchers.IO) {
+            preference.getCredentiales().collect {
+                withContext(Dispatchers.Main) {
+                    if (it.id == null) {
+                        val intent = Intent(this@DashboardClient, LoginActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                }
+            }
         }
     }
 }
